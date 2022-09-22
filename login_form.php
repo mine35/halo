4978099d1ccbf83ae43e8a8bbd12aca4f037a8f8
nanon <112994179+mine35@users.noreply.github.com>
2022-09-22 09:27:12 +0700

    Update login_form.php

--- login_form.php
+++ login_form.php
@@ -60,8 +60,6 @@ $label_captcha = "Confirmation Code";
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-
 	<!--end::Fonts-->
 	<!--begin::Page Vendor Stylesheets(used by this page)-->
 	<link href="<?php echo base_url(); ?>assets/plugins/custom/fullcalendar/fullcalendar.bundle.css" rel="stylesheet" type="text/css" />
@@ -192,7 +190,7 @@ $label_captcha = "Confirmation Code";
 					<!--end::Submit button-->
 					<div class="form-group">
 						<a class="form-control form-control-user" href="#" style="color: black;">
-							<i class="fa-brands fa-google"></i> Sign with google
+							<i class="fa-brands fa-google"></i>Sign with google
 						</a>
 					</div>
 				</div>
